714cfbef1fed24c093b5f74c8da9053a57e48020
BigMike216 <debangshubanik5@gmail.com>
2026-09-12 13:17:56 +0530

    Week4 Pratice Problem

---
+++ Week4/Assignment/Problem-1.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class Problem1 {
+    static int[] productExceptSelf(int[] nums) {
+        int[] answer = new int[nums.length];
+
+        int product = 1;
+        for (int i = 0; i < nums.length; i++) {
+            answer[i] = product;
+            product *= nums[i];
+        }
+
+        product = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            answer[i] *= product;
+            product *= nums[i];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+    }
+}
